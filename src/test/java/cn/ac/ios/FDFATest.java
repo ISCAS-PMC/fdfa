c89051b9e1de1b21d180d2e9750510183f465930
lijianlin <lijianlin1995@gmail.com>
2017-11-24 22:16:18 +0800

    Deprecate ProductDFA and ProductState.

--- src/test/java/cn/ac/ios/FDFATest.java
+++ src/test/java/cn/ac/ios/FDFATest.java
@@ -6,25 +6,26 @@ import cn.ac.ios.machine.dfa.*;
 import java.util.ArrayList;
 
 public class FDFATest {
-    public static void main(String[] argv){
-//        Complement tested with example_FDFA1
-//        FDFA F1 = DFAGen.exmple_FDFA1();
-//        F1 = BasicOperations.complement(F1);
-//        DFA M = F1.leadingDFA;
-//        System.out.println(M);
-//        for (DFA progess : F1.progressDFAs){
-//            System.out.println(progess);
-//        }
+
+    public static void ComplementTest() {
+        FDFA F1 = DFAGen.exmple_FDFA1();
+        F1 = BasicOperations.complement(F1);
+        DFA M = F1.leadingDFA;
+        System.out.println(M);
+        for (DFA progess : F1.progressDFAs){
+            System.out.println(progess);
+        }
+    }
+
+    public static void InterSectionTest() {
         FDFA F1 = DFAGen.exmple_FDFA1();
         FDFA F2 = DFAGen.exmple_FDFA2();
         FDFA C = BasicOperations.intersection(F1,F2);
         System.out.println(C);
-//        F1 = BasicOperations.complement(F1);
-//        DFA M = F1.leadingDFA;
-//        System.out.println(M);
-//        for (DFA progess : F1.progressDFAs){
-//            System.out.println(progess);
-//        }
+    }
+
+    public static void main(String[] argv){
+        InterSectionTest();
     }
 
 
